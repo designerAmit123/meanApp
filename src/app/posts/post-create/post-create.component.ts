0c1d4956f2a8987352a12b6f5f985af54d8b5ae3
designerAmit123 <amitkadu7@gmail.com>
2021-12-29 00:29:36 +0530

    updated post-create-component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,5 +5,9 @@ import { Component } from "@angular/core";
   templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
+  newPost = "No Content";
 
+  onAddPost(){
+    this.newPost = "User's first Post";
+  }
 }
